00de960e6b68fab4e4c2f60ed2910e00f8c63ecb
jakefish18 <jakefish.work@gmail.com>
2024-09-06 22:09:09 +0300

    Adding error output for bodyParser error in /register

--- app/handlers/auth.go
+++ app/handlers/auth.go
@@ -36,7 +36,7 @@ func Register(c *fiber.Ctx, db *sql.DB) error {
 
 	if err := c.BodyParser(userRegisterData); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid body parameters",
+			"error": err.Error(),
 		})
 	}
 
