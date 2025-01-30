02412d4104fe34d257ba2765e9279d94eabe7646
jakefish18 <jakefish.work@gmail.com>
2025-01-30 12:28:32 +0300

    Add get_user dependency

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,15 +1,20 @@
 """
 Dependencies for FastAPI dependency injection system.
 """
+from typing import Generator, Annotated
 
-from typing import Generator
-
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 from database import SessionLocal
+from models import User, AccessToken
+from crud import CrudAccessToken, CrudUser
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+crud_access_token = CrudAccessToken(AccessToken)
+crud_user = CrudUser(User)
 
 
 def get_db() -> Generator:
@@ -28,3 +33,28 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    access_token_str: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
+) -> User:
+    """
+    This dependency injection used for getting user,
+    which has sent requests with access token.
+    Token must be passed in headers.
+
+    Parameters:
+        token: Annotated[str, Depends(oatuh2_scheme)] - access token from headers.
+        db: Session - SQLAlchemy session to database, initializing in dependency injection.
+
+    Returns:
+        user: models.User - user sqlalchemy model.
+        HTTPExecption(401) if invalid access token.
+    """
+    access_token: AccessToken = crud_access_token.get_by_token(db, access_token_str)
+    if not access_token:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED, detail="Invalid access token."
+        )
+
+    return access_token.user
